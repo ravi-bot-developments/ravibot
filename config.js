d0a3d5f2e92681a8592e67000c6f315c3ee3c60a
Jeffrey <68307390+Jeffreyz1129@users.noreply.github.com>
2021-11-10 13:41:22 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ export const PREFIX = process.env.PREFIX?.length ? process.env.PREFIX : null
 export const STATUS = process.env.STATUS?.length ? process.env.STATUS : null
 export const TOKEN = process.env.TOKEN?.length ? process.env.TOKEN : null
 export const PRESENCE_STATUS = (process.env.PRESENCE_STATUS?.length && ['dnd', 'idle', 'invisible', 'online'].includes(process.env.PRESENCE_STATUS) ? process.env.PRESENCE_STATUS : 'online') as 'dnd' | 'idle' | 'invisible' | 'online'
-export const TOKEN = process.env.GUID_ID?.length ? process.env.GUILD_ID : null
-export const TOKEN = process.env.OWNERS?.length ? process.env.OWNERS : null
+export const GUILD_ID = process.env.GUID_ID?.length ? process.env.GUILD_ID : null
+export const OWNERS = process.env.OWNERS?.length ? process.env.OWNERS : null
